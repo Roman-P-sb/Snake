b79148554e4fb5d49cad406e9456585718036bd6
Роман <pankratov.76@mail.ru>
2022-01-02 10:50:12 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 380);// 320 345
+        setLocation(400, 400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main mw = new Main();
+    }
+}
